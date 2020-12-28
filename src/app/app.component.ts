0a175c7ef80815b3b0c5e4dd07898a55db4938da
da5407 <da5407@gmail.com>
2020-12-28 18:42:42 +0800

    done multi-language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'translate-app';
+  nameError = false;
+  ret: string = 'en-us';
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en-us', 'zh-tw']);
+    translate.setDefaultLang(this.getLang());
+  }
+
+  switchLang(lang: string) {
+    localStorage.setItem('lang', lang);
+    this.translate.use(lang);
+  }
+  checkName(usrName: string) {
+    console.log(usrName);
+    this.nameError = usrName === '';
+  }
+
+  getLang(): string {
+    const str =
+      localStorage.getItem('lang') === null
+        ? this.ret
+        : localStorage.getItem('lang');
+    this.ret = str === null ? 'en-us' : str;
+    return this.ret;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+// AOT compilation support
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
